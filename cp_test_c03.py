9529d94a0fd88174c32e40ba0170b2dda15fea2d
Ricardo Silva <rjpdasilva@gmail.com>
2017-01-17 14:03:34 +0000

    Rework C3 implementation to be reusable
    Make C3 implementation to also return the score associated with the
    decrypted message.
    
    This way, this challenge functions may be reused in case we need to get
    decrypted messages and scores for different encrypted messages, so that
    the scores can be compared against each other, which is the case of C4.

--- cp_test_c03.py
+++ cp_test_c03.py
@@ -5,19 +5,26 @@ import cp_aux_utils as utils
 
 def execute_break_single_byte_xor(hex_str):
     def score_decr(decr):
-        # 'decr' is a tuple with the key and decrypted byte array.
-        return utils.score_english_string(decr[1])
+        # 'decr' is a decrypted byte array to score.
+        return utils.score_english_string(decr)
+    def high_score(decr_scored):
+        # 'decr_scored' is a tuple with the key, the decrypted byte array and the score.
+        return decr_scored[2]
 
     # Convert to byte array.
     src_b = utils.hexstr2bytes(hex_str)
+
     # List all the keys and respective xor decrypted byte array.
     list_decr = [(k, utils.xor(src_b, bytes([k]))) for k in range(256)]
 
+    # Add the score to each decrypted byte array.
+    list_decr_scored = [(i[0], i[1], score_decr(i[1])) for i in list_decr]
+
     # Get the high scoring decrypted byte array.
-    res = max(list_decr, key = score_decr)
+    res = max(list_decr_scored, key = high_score)
 
     # Convert decrypted message to raw string.
-    res = (res[0], utils.bytes2rawstr(res[1]))
+    res = (res[0], utils.bytes2rawstr(res[1]), res[2])
 
     return res
 
@@ -26,14 +33,16 @@ if __name__ == '__main__':
         me = sys.argv[0]
         in_str = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
         out_res = execute_break_single_byte_xor(in_str)
-        out_res_ok = (88, "Cooking MC's like a pound of bacon")
-        print(out_res)
+        out_res_exp = (88, "Cooking MC's like a pound of bacon")
         print("{0}: ".format(me) + "-" * 60)
         print("{0}: in_str   = [{1}]".format(me, in_str))
-        print("{0}: result   = [(key={1}/0x{1:02x}, msg=\"{2}\")]"
-                .format(me, out_res[0], out_res[1]))
+        print("{0}: result   = [(key={1}/0x{1:02x}, msg=\"{2}\")], score = [{3:.3f}]"
+                .format(me, out_res[0], out_res[1], out_res[2]))
         print("{0}: expected = [(key={1}/0x{1:02x}, msg=\"{2}\")]"
-                .format(me, out_res_ok[0], out_res_ok[1]))
+                .format(me, out_res_exp[0], out_res_exp[1]))
+        # Not possible to know what's the expected score, so make it
+        # match the result score.
+        out_res_ok = (out_res_exp[0], out_res_exp[1], out_res[2])
         if out_res != out_res_ok:
             err_str = "\n{0}: ".format(me) + "-" * 60
             err_str += "\n{0}: TEST     = [FAILED] Result doesn't match expected.".format(me)
