e107cf14b98f31e10e775fd742e4a1a9977c0a33
Ricardo Silva <rjpdasilva@gmail.com>
2017-01-19 12:05:39 +0000

    Implement Challenge 7: AES in ECB mode
    Add script for implementing Challenge 7.
    
    Use same script structure as in previous challenges implementation.
    The challenge input data was placed in a new data file 'data_c7.txt'.
    
    The expected results are not declared in the challenge's statement.
    They came out from test execution and debugging.
    The results seem correct, as they turned out to be the same as in
    challenge 6 (followed by 4 extra 0x04 bytes due to padding maybe?). This
    way, used 'data_c6_out.txt' for checking the results.
    
    The 'get_ciphertext()' function from C6 was reused in this challenge for
    getting the ciphertext, which is base64 encoded in a text file.
    
    The challenge depends on the ''Crypto.Util.AES' module for accessing the
    AES functionality required by this challenge.

---
+++ cp_test_c07.py
@@ -0,0 +1,67 @@
+"""Cryptopals Challenges: Test Challenge 07: AES in ECB mode."""
+
+import sys
+import cp_aux_utils as utils
+import cp_test_c06 as c6
+from Crypto.Cipher import AES
+
+title = "Challenge 07: AES in ECB mode"
+
+def execute_decrypt_aes_ecb(file_name, key):
+    """Descrypt a AES-ECB encrypted and base64 encoded file using a key."""
+
+    # Get the ciphertext.
+    ct = c6.get_ciphertext(file_name)
+
+    # Convert the key to byte array.
+    key_b = utils.rawstr2bytes(key)
+
+    # Decrypt.
+    crypto = AES.new(key_b, AES.MODE_ECB)
+    plaintext = crypto.decrypt(ct)
+    print(plaintext)
+
+    return utils.bytes2rawstr(plaintext)
+
+if __name__ == '__main__':
+    try:
+        me = sys.argv[0]
+        in_file = 'data_c7.txt'
+        in_key = "YELLOW SUBMARINE"
+        out_res = execute_decrypt_aes_ecb(in_file, in_key)
+        out_file = 'data_c6_out.txt'
+        out_res_ok = utils.file_get(out_file)
+        # These extra bytes (padding?) are part of the decrypted message.
+        out_res_ok += ("\x04" * 4)
+        print("{0}: ".format(me) + "-" * 60)
+        print("{0}: {1}".format(me, title))
+        print("{0}: ".format(me) + "-" * 60)
+        print("{0}: in_file  = [{1}]".format(me, in_file))
+        print("{0}: in_key   = [{1}]".format(me, in_key))
+        print("{0}: ".format(me) + "-" * 60)
+        print("{0}: result   = [<see below>)]".format(me))
+        print("{0}: ".format(me) + "-" * 60)
+        print(out_res)
+        print("{0}: ".format(me) + "-" * 60)
+        print("{0}: expected = [<see below>)]".format(me))
+        print("{0}: ".format(me) + "-" * 60)
+        print(out_res_ok)
+        if out_res != out_res_ok:
+            err_str = "\n{0}: ".format(me) + "-" * 60
+            err_str += "\n{0}: TEST     = [FAILED] Result doesn't match expected.".format(me)
+            err_str += "\n{0}: ".format(me) + "-" * 60
+            raise Exception(err_str)
+        print("{0}: ".format(me) + "-" * 60)
+        print("{0}: TEST     = [OK]".format(me))
+        print("{0}: ".format(me) + "-" * 60)
+    except Exception:
+        print("{0}: ".format(me) + "-" * 60)
+        print("{0}: Caught ERROR EXCEPTION:".format(me))
+        raise
+    except:
+        print("{0}: ".format(me) + "-" * 60)
+        print("{0}: Caught UNEXPECTED EXCEPTION:".format(me))
+        raise
+
+    sys.exit(0)
+
