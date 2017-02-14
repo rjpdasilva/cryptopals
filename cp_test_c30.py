6c99265ac6eaa1a2b457a0c25f2c1d17cd507ca7
Ricardo Silva <rjpdasilva@gmail.com>
2017-02-14 17:57:10 +0000

    Fix output messages (cosmetics)
    Fixed the alignment of test result output messages in Challenges 28, 29
    and 30.

--- cp_test_c30.py
+++ cp_test_c30.py
@@ -189,11 +189,11 @@ if __name__ == '__main__':
             return True
         if not result_ok():
             err_str = "\n{0}: ".format(me) + "-" * 60
-            err_str += "\n{0}: TEST     = [FAILED] Result doesn't match expected.".format(me)
+            err_str += "\n{0}: TEST          = [FAILED] Result doesn't match expected.".format(me)
             err_str += "\n{0}: ".format(me) + "-" * 60
             raise Exception(err_str)
         print("{0}: ".format(me) + "-" * 60)
-        print("{0}: TEST     = [OK]".format(me))
+        print("{0}: TEST          = [OK]".format(me))
         print("{0}: ".format(me) + "-" * 60)
     except Exception:
         print("{0}: ".format(me) + "-" * 60)
